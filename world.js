356c66f64be805dfabd8f11f93c8a75b5788f439
Javon Ellis <javon.ellis@aye-tech.com>
2020-11-14 17:57:14 -0500

    added fetch request to display countries

--- world.js
+++ world.js
@@ -1,3 +1,29 @@
 window.onload = function(){
     
+
+    $('#lookup').on('click', function(){
+       let result = document.getElementById('result');
+       
+        let country = $('#country').val();
+        fetchCountry(country,function(data){
+            console.log(data)
+            result.innerHTML = data
+
+        });
+
+        
+    });
+}
+
+function fetchCountry(country, callback){
+    $.ajax({
+        url:"http://localhost/info2180-lab5/world.php",
+        type: "GET",
+        data: {
+            country: country,
+          },
+        success: function (data){
+            callback(data)
+        }
+    })
 }
