0dee1c0f86bf32026dead71e47c41385732e01b6
Javon Ellis <javon.ellis@aye-tech.com>
2020-11-14 19:20:31 -0500

    edit world.php to return data in table

--- world.php
+++ world.php
@@ -13,8 +13,20 @@ $stmt = $conn->query("SELECT * FROM countries WHERE name LIKE '%$country%'");
 $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
 ?>
-<ul>
-<?php foreach ($results as $row): ?>
-  <li><?= $row['name'] . ' is ruled by ' . $row['head_of_state']; ?></li>
-<?php endforeach; ?>
-</ul>
+
+<table class="table table-hover">
+  <tr>
+    <th>Country Name</th>
+    <th>Continent</th>
+    <th>Independece Year</th>
+    <th>Head of State</th>
+  </tr>
+  <?php foreach ($results as $row): ?>
+  <tr>
+    <td><?= $row['name']?></td>
+    <td><?= $row['continent']?></td>
+    <td><?= $row['independence_year']?></td>
+    <td><?=$row['head_of_state']?></td>
+  </tr>
+  <?php endforeach; ?>
+</table>
